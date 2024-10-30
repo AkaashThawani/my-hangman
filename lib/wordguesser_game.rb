82e62fc592b958c8ec6df35946b8a358882294d8
Akaash Thawani <akaashthawani123@gmail.com>
2024-10-29 23:47:20 -0400

    code fixes

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -1,71 +1,48 @@
 class WordGuesserGame
-
-  # add the necessary class methods, attributes, etc. here
-  # to make the tests in spec/wordguesser_game_spec.rb pass.
-
-  # Get a word from remote "random word" service
-
-  attr_accessor :word,:guess,:wrong_guesses
+  attr_accessor :word, :correct_guesses, :wrong_guesses
 
   def initialize(word)
-    @word = word
-    @guesses = []
-    @wrong_guesses = []
-
+    @word = word.downcase
+    @correct_guesses = ''
+    @wrong_guesses = ''
   end
 
-  # You can test it by installing irb via $ gem install irb
-  # and then running $ irb -I. -r app.rb
-  # And then in the irb: irb(main):001:0> WordGuesserGame.get_random_word
-  #  => "cooking"   <-- some random word
   def self.get_random_word
     require 'uri'
     require 'net/http'
     uri = URI('http://randomword.saasbook.info/RandomWord')
-    Net::HTTP.new('randomword.saasbook.info').start { |http|
+    Net::HTTP.new('randomword.saasbook.info').start do |http|
       return http.post(uri, "").body
-   }
+    end
   end
 
-  def guess(word)
-	
-	if (word.nil? or word == '' or /[^A-Za-z]/.match(word)!= nil)
-		raise ArgumentError.new("Not a valid word")
-	end 
-		
+  def guess(letter)
+    raise ArgumentError.new("Not a valid letter") if letter.nil? || letter.empty? || !letter.match?(/[A-Za-z]/)
 
-	if word != nil
-		word=word.downcase
-	end
-	  
-	if (@guesses.include? word or @wrong_guesses.include? word)
-		return false
-	end
-	
-	if @word.include? word
-		@guesses += word
-		return true
-	else
-		@wrong_guesses += word
-		return true
-	end
+    letter = letter.downcase
+
+    return false if @correct_guesses.include?(letter) || @wrong_guesses.include?(letter)
+
+    if @word.include?(letter)
+      @correct_guesses += letter
+      true
+    else
+      @wrong_guesses += letter
+      true
+    end
   end
-  
- 
+
+  def guesses
+    @correct_guesses
+  end
+
   def word_with_guesses
-	temp = @guesses
-	return @word.gsub(/[^#{temp}]/,'-')
+    @word.chars.map { |char| @correct_guesses.include?(char) ? char : '-' }.join
   end
 
   def check_win_or_lose
-	  
-	if (@wrong_guesses.length >= 7)
-		return :lose
-	else if (word_with_guesses == @word)
-		return :win
-	else
-		return :play
-	end
+    return :lose if @wrong_guesses.length >= 7
+    return :win if word_with_guesses == @word
+    :play
   end
-
 end
